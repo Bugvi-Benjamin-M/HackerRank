98dc371f37066ff7eb87ab10c694e5f0d5f974dc
Búgvi Magnussen <bugvimagnussen@gmail.com>
2019-10-16 23:19:02 +0100

    Some basic SQL.

---
+++ SQL/BasicSelet.sql
@@ -0,0 +1,26 @@
+# All SQL queries are of the Mysql dialect
+
+# Revising the Select Query I
+SELECT * FROM city WHERE COUNTRYCODE = "USA" AND POPULATION > 100000;
+
+# Revising the Select Query II
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
+
+# Select All
+SELECT * FROM CITY;
+
+# Select By ID
+SELECT * FROM CITY WHERE ID = 1661;
+
+# Japanese Cities' Attributes
+SELECT * FROM CITY WHERE COUNTRYCODE = "JPN";
+
+# Japanese Cities' Names
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "JPN";
+
+# Weather Observation Station 1
+SELECT CITY, STATE FROM STATION;
+
+# Weather Observation Station 3
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
+
